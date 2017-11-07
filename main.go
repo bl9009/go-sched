e6f709641a97d59c7f4f61319f1024a41420ae28
Benjamin Labas <benjamin.labas@airbus.com>
2017-11-07 19:20:10 +0100

    save work

--- main.go
+++ main.go
@@ -8,14 +8,29 @@ func main() {
 	terminating := false
 	clockRate := int64(1000000)
 
+    drift := make(chan int64)
+    var accDrift int64
+    var drifts int
+
 	var wg sync.WaitGroup
 
 	models := append([]*Model{}, NewModel("a", 5000000), NewModel("b", 7000000), NewModel("c", 12000000), NewModel("d", 9000000))
 
 	//var next *Model
 
+    go func() {
+        for {
+            fmt.Println("waiting for drift...")
+            lastDrift := <- drift
+            fmt.Printf("received drift %d", lastDrift)
+            accDrift += <- drift
+            drifts += 1
+            fmt.Printf("avg drift: %f\n", float32(accDrift) / float32(drifts))
+        }
+    }()
+
 	for !terminating {
-		fmt.Printf("scheduling @%d...\n", microseconds())
+		//fmt.Printf("scheduling @%d...\n", microseconds())
 
 		time.Sleep(time.Duration(clockRate) * time.Microsecond)
 
@@ -24,15 +39,28 @@ func main() {
 
 			now := microseconds()
 
-			if model.lastCycle+model.cycleTime < now+clockRate {
+            nextCycle := model.lastCycle + model.cycleTime
+            nextTick := now + clockRate
 
-				model.lastCycle = now
+			if  nextCycle < nextTick {
 
-				go func(model *Model) {
+				go func(model *Model, drift chan int64) {
+                    time.Sleep(time.Duration(nextCycle - microseconds()))
+
+                    localDrift := nextCycle - microseconds()
+
+                    if model.lastCycle != 0 {
+                        drift <- localDrift
+                    }
+
+                    model.lastCycle = microseconds()
+
+                    //fmt.Printf("planned: %d, scheduled: %d, drift: %d\n", nextCycle, microseconds(), drift)
+
 					defer wg.Done()
 
 					model.Run()
-				}(model)
+				}(model, drift)
 			}
 		}
 	}
